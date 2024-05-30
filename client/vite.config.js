d767811dc12ec15098ab465ef1cd976c22fa3be1
Khoi Phan <jasonphan2631@gmail.com>
2024-05-29 20:54:32 -0700

    33

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,25 +1,46 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  },
   server: {
     port: 3000,
     open: true,
     proxy: {
-      '/graphql': {
+      '/graphql' : {
         target: 'http://localhost:3001',
+        changeOrigin: true,
         secure: false,
-        changeOrigin: true
-      }
-    }
+      },
+    },
   },
-  test: {
-    globals: true,
-    environment: 'happy-dom'
-  }
-})
+});
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'dist'
+//   },
+//   server: {
+//     port: 3000,
+//     open: true,
+//     proxy: {
+//       '/graphql': {
+//         target: 'http://localhost:3001',
+//         secure: false,
+//         changeOrigin: true
+//       }
+//     }
+//   },
+//   test: {
+//     globals: true,
+//     environment: 'happy-dom'
+//   }
+// })
