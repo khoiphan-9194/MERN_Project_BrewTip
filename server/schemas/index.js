d3ae704253b686d267d378ff3296f86cb041fb41
Khoi Phan <jasonphan2631@gmail.com>
2024-05-31 23:43:07 -0700

    heo map

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -3,17 +3,4 @@ const resolvers = require('./resolvers');
 
 
 
-
-
-
-    // const session = await stripe.checkout.sessions.create({
-    //   payment_method_types: ['card'],
-    //   line_items,
-    //   mode: 'payment',
-    //   success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
-    //   cancel_url: `${url}/`,
-    // });
-
-    // return { session: session.id };
-
 module.exports = { typeDefs, resolvers };
