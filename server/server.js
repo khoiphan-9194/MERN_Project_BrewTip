5d87063a058245986c207d996286a7a04915f8ab
Khoi Phan <jasonphan2631@gmail.com>
2024-06-03 20:42:54 -0700

    heu

--- server/server.js
+++ server/server.js
@@ -3,67 +3,41 @@ const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
-const multer = require('multer');
-const cors = require('cors');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 const app = express();
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+    typeDefs,
+    resolvers,
 });
 
-// Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
-  await server.start();
+    await server.start();
 
-  app.use(express.urlencoded({ extended: false }));
-  app.use(express.json());
-  app.use(cors());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
 
-  app.use('/graphql', expressMiddleware(server, {
-    context: authMiddleware
-  }));
+    app.use('/graphql', expressMiddleware(server, {
+        context: authMiddleware
+    }));
 
-  const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      return cb(null, '../client/public/uploads');
-    },
+    if(process.env.NODE_ENV === 'production') {
+        app.use(express.static(path.join(__dirname, '../client/dist')));
 
-    
-    filename: (req, file, cb) => {
-     return cb(null, `${file.originalname}`);
+        app.get('*', (req, res) => {
+            res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+        });
     }
-  });
 
-  const upload = multer({ storage: storage });
-  app.post('/upload', upload.single('file'), (req, res) => {
- console.log(req.body);
-    console.log(req.file);
-  });
-
-
-
-  
-
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
-
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    db.once('open', () => {
+        app.listen(PORT, () => {
+            console.log(`API server running on port ${PORT}!`);
+            console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+        });
     });
-  }
-
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-    });
-  });
 };
 
-// Call the async function to start the server
-  startApolloServer();
+startApolloServer();
