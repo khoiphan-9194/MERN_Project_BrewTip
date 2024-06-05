49f559b881167729babb7b89a18533b2c9092f99
Khoi Phan <jasonphan2631@gmail.com>
2024-06-05 15:09:04 -0700

    333

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,17 @@
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  },
   server: {
     port: 3000,
     open: true,
     proxy: {
       '/graphql': {
-        target: `http://localhost:${process.env.PORT || 3001}`, 
+        target: 'http://localhost:3001',
+        changeOrigin: true,
         secure: false,
-        changeOrigin: true
       }
     }
   }
