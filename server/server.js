c6c78abb7c21c3a6fdbfe7974eb11ecf2558c316
Khoi Phan <jasonphan2631@gmail.com>
2024-06-08 23:16:37 -0700

    33

--- server/server.js
+++ server/server.js
@@ -51,7 +51,7 @@ app.post('/upload', upload.single('file'), (req, res) => {
  
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'MERN-Project')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
 
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
